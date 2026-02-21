edd6e3948d3530b4b3bc09d9135e07f42eb2ca55
Yamin Einzali <yamineinzali54@gmail.com>
2026-02-21 04:22:50 -0700

    Update ExpenseList.tsx

--- components/ExpenseList.tsx
+++ components/ExpenseList.tsx
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import { motion, AnimatePresence } from 'motion/react';
+import { motion, AnimatePresence } from 'framer/react';
 import { Trash2, Receipt, Calendar } from 'lucide-react';
 import { Expense } from '../types';
 
