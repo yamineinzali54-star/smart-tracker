76a095dfaa1c2d3a221209e90ef91fbbb4bb4c84
Yamin Einzali <yamineinzali54@gmail.com>
2026-02-21 04:28:21 -0700

    Update Summary.tsx

--- components/Summary.tsx
+++ components/Summary.tsx
@@ -1,6 +1,6 @@
 
 import React, { useMemo } from 'react';
-import { motion } from 'framer/react';
+import { motion } from 'framer-motion';
 import { Wallet, TrendingUp, RefreshCcw } from 'lucide-react';
 import { Expense } from '../types';
 
